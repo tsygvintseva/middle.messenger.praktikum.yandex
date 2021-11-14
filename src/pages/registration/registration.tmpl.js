48ac3e417e3f54c4fce377792940510c63788891
etsygvintseva <e.tsygvintseva@polymatica.com>
2021-11-14 11:30:34 +0300

    fix class name

--- src/pages/registration/registration.tmpl.js
+++ src/pages/registration/registration.tmpl.js
@@ -1,7 +1,7 @@
 export default `
   <div class="container">
-    <section class="modal login">
-      <h1 class="login__title">{{ title }}</h1>
+    <section class="modal registration">
+      <h1 class="registration__title">{{ title }}</h1>
       <form action="" method="post" class="form">
       
         <div class="form__content">
